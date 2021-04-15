aa93626ae58263bb133bc475beaa16b0cfc5306b
Neradoc <neraOnGit@ri1.fr>
2021-04-15 19:04:17 +0200

    live demo of the latest support matrix filter

--- support_matrix_filter/Module-Support-Matrix/documentation_options.js
+++ support_matrix_filter/Module-Support-Matrix/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.2.0-beta.3',
-    LANGUAGE: 'en',
+    VERSION: '6.2.0',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
